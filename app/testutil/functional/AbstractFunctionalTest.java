44c7c97b7927db9f82e901dc8ed62af36e0bbd9f
Ben Verbeken <ben@ostia.be>
2012-02-11 22:14:32 +0100

    removed unneccessary method and files

--- app/testutil/functional/AbstractFunctionalTest.java
+++ app/testutil/functional/AbstractFunctionalTest.java
@@ -4,8 +4,6 @@ import org.junit.Before;
 import play.mvc.Http;
 import play.test.FunctionalTest;
 
-import static play.mvc.Http.StatusCode.FORBIDDEN;
-
 public abstract class AbstractFunctionalTest extends FunctionalTest {
 	
 	protected FunctionalAssert assertThat(Http.Response response){
@@ -16,10 +14,5 @@ public abstract class AbstractFunctionalTest extends FunctionalTest {
     public void logoutBeforeTest() {
         GET("/logout");
     }
-
-	public static void assertIsForbidden(Http.Response response) {
-		assertStatus(FORBIDDEN, response);
-	}
-
 
 }
