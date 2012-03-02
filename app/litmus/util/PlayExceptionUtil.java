866cb31fe38ce3a5ec4d2a484780e2a8f4eec8f4
Ben Verbeken <ben@ostia.be>
2012-03-02 07:46:00 +0100

    any result will do, not just NotFound

--- app/litmus/util/PlayExceptionUtil.java
+++ app/litmus/util/PlayExceptionUtil.java
@@ -1,12 +1,12 @@
 package litmus.util;
 
-import play.mvc.results.NotFound;
+import play.mvc.results.Result;
 
 public class PlayExceptionUtil {
 
 	public static RuntimeException tryToFindNotFound(Throwable e) {
 		if (e.getCause() != null) {
-			if (e.getCause() instanceof NotFound) {
+			if (e.getCause() instanceof Result) {
 				return (RuntimeException) e.getCause();
 			} else {
 				return tryToFindNotFound(e.getCause());
