8b90c34dd0ca039cf0460254930d717aacfaf29e
Ben Verbeken <ben@ostia.be>
2012-03-06 18:29:01 +0100

    added login method

--- app/litmus/functional/FunctionalTest.java
+++ app/litmus/functional/FunctionalTest.java
@@ -4,12 +4,20 @@ public abstract class FunctionalTest extends FestAssertFunctionalTest {
 
 	protected static Response get(Object url) {
 		return new Request(url).get();
-
 	}
 
 	protected static Html getHtml(Object url) {
 		return get(url).getHtml();
 	}
+
+	protected Response login(String userName, String password) {
+		// TODO: check whether Secure module is available
+		return new Request("/login")
+				.with("username", userName)
+				.with("password", password)
+				.post();
+	}
+
 
 	// TODO: add post, put, head and delete methods
 
