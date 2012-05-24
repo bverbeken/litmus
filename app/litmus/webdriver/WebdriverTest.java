5be71ecef78573e5d7d2c8e6042fb3a2457bfb1b
Ben Verbeken <ben@ostia.be>
2012-05-24 07:07:53 +0200

    added check for play.pool configuration

--- app/litmus/webdriver/WebdriverTest.java
+++ app/litmus/webdriver/WebdriverTest.java
@@ -1,6 +1,21 @@
 package litmus.webdriver;
 
 import litmus.functional.FestAssertFunctionalTest;
+import org.junit.BeforeClass;
+import play.Play;
+
+import static java.lang.Integer.parseInt;
 
 public abstract class WebdriverTest extends FestAssertFunctionalTest {
+
+    @BeforeClass
+    public static void checkPlayConfig() {
+        String playPool = Play.configuration.getProperty("play.pool");
+        if (playPool == null) {
+            throw new IllegalStateException("'play.pool' property not found. Please set the 'play.pool' property in your application.conf to at least 2");
+        } else if (parseInt(playPool) < 2) {
+            throw new IllegalStateException("Cannot run webdriver tests when 'play.pool' config value is < 2. Please set the 'play.pool' property in your application.conf to at least 2");
+        }
+    }
+
 }
