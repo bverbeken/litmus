8d039bd2efeea20b94a50a731b406e16bb256273
Ben Verbeken <ben@ostia.be>
2012-05-25 07:59:03 +0200

    first test lists

--- app/controllers/LitmusTestRunner.java
+++ app/controllers/LitmusTestRunner.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import com.beust.jcommander.internal.Maps;
+import litmus.engine.TestEngine;
 import play.mvc.Controller;
+
+import java.util.List;
+import java.util.Map;
 
 public class LitmusTestRunner extends Controller {
 
-    public static void showTests(){
-        render();
+    public static void testList(){
+        Map<String, List<Class>> tests = Maps.newHashMap();
+        tests.put("UNIT", TestEngine.allUnitTests());
+        tests.put("FUNCTIONAL", TestEngine.allFunctionalTests());
+        render(tests);
     }
 
 }
