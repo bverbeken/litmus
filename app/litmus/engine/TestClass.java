62a76e14ee5b19dfd30237c27ddd063f60b1cd97
Ben Verbeken <ben@ostia.be>
2012-05-29 16:58:21 +0200

    added TestClass domain class, failing tests doesn't stop runner from running + removed autoscrolling to failed test

--- app/litmus/engine/TestClass.java
+++ app/litmus/engine/TestClass.java
@@ -1,5 +1,18 @@
 package litmus.engine;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.List;
+
+import static com.google.common.collect.Collections2.filter;
+import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Collections.sort;
+
 public class TestClass {
 
     private final Class clazz;
@@ -11,4 +24,23 @@ public class TestClass {
     public String getName() {
         return clazz.getName();
     }
+
+    public List<String> getTestMethodNames() {
+        List<String> list = newArrayList(transform(getTestMethods(), new Function<Method, String>() {
+            public String apply(Method method) {
+                return method.getName();
+            }
+        }));
+        sort(list);
+        return list;
+    }
+
+    private Collection<Method> getTestMethods() {
+        return filter(newArrayList(clazz.getMethods()), new Predicate<Method>() {
+            public boolean apply(Method method) {
+                return method.getAnnotation(Test.class) != null;
+            }
+        });
+    }
+
 }
