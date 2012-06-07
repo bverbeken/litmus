e01ed51b84f98b30974b87fab2b6284d2506fba2
Ben Verbeken <ben@ostia.be>
2012-06-07 19:47:45 +0200

    testrunner cleanup

--- app/controllers/LitmusTestRunner.java
+++ app/controllers/LitmusTestRunner.java
@@ -2,18 +2,20 @@ package controllers;
 
 import litmus.engine.Tests;
 import play.Logger;
-import play.Play;
-import play.libs.IO;
 import play.mvc.Controller;
-import play.templates.Template;
-import play.templates.TemplateLoader;
 import play.test.TestEngine;
 
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings("UnusedDeclaration")
+import static play.Play.getFile;
+import static play.libs.IO.writeContent;
+import static play.templates.TemplateLoader.load;
+import static play.test.TestEngine.TestResults;
+
+
+@SuppressWarnings({"UnusedDeclaration", "ConstantConditions"})
 public class LitmusTestRunner extends Controller {
 
     public static void testList() {
@@ -22,7 +24,7 @@ public class LitmusTestRunner extends Controller {
     }
 
     public static void init() {
-        File testResults = Play.getFile("test-result");
+        File testResults = getFile("test-result");
         if (!testResults.exists()) {
             testResults.mkdir();
         }
@@ -35,34 +37,41 @@ public class LitmusTestRunner extends Controller {
     }
 
     public static void end() {
-        File testResults = Play.getFile("test-result/result." + params.get("result"));
-        IO.writeContent(params.get("result"), testResults);
+        File testResults = getFile("test-result/result." + params.get("result"));
+        writeContent(params.get("result"), testResults);
         renderText("done");
     }
 
-    public static void run(String test) throws Exception {
-        Play.getFile("test-result").mkdir();
-        TestEngine.TestResults results = TestEngine.run(test.substring(0, test.length() - 6));
+    public static void run(String test) {
+        getFile("test-result").mkdir();
+        TestResults results = TestEngine.run(test.substring(0, test.length() - 6));
+        Map<String, Object> options = makeOptions(test, results);
+        String result = load("LitmusTestRunner/results.html").render(options);
+        File resultsFile = getFile("test-result/" + test + (results.passed ? ".passed" : ".failed") + ".html");
+        writeContent(result, resultsFile);
+        writeXmlOutput(options);
         response.status = results.passed ? 200 : 500;
-        Template resultTemplate = TemplateLoader.load("LitmusTestRunner/results.html");
+        response.contentType = "text/html";
+        renderText(result);
+    }
+
+    private static Map<String, Object> makeOptions(String test, TestResults results) {
         Map<String, Object> options = new HashMap<String, Object>();
         options.put("test", test);
         options.put("results", results);
-        String result = resultTemplate.render(options);
-        File testResults = Play.getFile("test-result/" + test + (results.passed ? ".passed" : ".failed") + ".html");
-        IO.writeContent(result, testResults);
+        return options;
+    }
+
+    private static void writeXmlOutput(Map<String, Object> options) {
         try {
-            // Write xml output
             options.remove("out");
-            resultTemplate = TemplateLoader.load("LitmusTestRunner/results-xunit.xml");
-            String resultXunit = resultTemplate.render(options);
-            File testXunitResults = Play.getFile("test-result/TEST-" + test.substring(0, test.length() - 6) + ".xml");
-            IO.writeContent(resultXunit, testXunitResults);
+            String test  = options.get("test").toString();
+            String resultXunit = load("LitmusTestRunner/results-xunit.xml").render(options);
+            File testXunitResults = getFile("test-result/TEST-" + test.substring(0, test.length() - 6) + ".xml");
+            writeContent(resultXunit, testXunitResults);
         } catch (Exception e) {
             Logger.error(e, "Cannot ouput XML unit output");
         }
-        response.contentType = "text/html";
-        renderText(result);
     }
 
 
