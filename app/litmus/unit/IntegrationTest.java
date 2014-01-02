4cc9a5c08ad36e241a23ee85fed1b713a75c437c
Matti Roloux <matti.roloux@gmail.com>
2014-01-02 16:44:36 +0100

    Incoming API requests are logged when booking and releasing seats

--- app/litmus/unit/IntegrationTest.java
+++ app/litmus/unit/IntegrationTest.java
@@ -2,10 +2,15 @@ package litmus.unit;
 
 import org.junit.Before;
 
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static play.db.jpa.JPA.em;
 import static play.test.Fixtures.deleteAllModels;
 
 public abstract class IntegrationTest extends UnitTest {
 
+    protected <T> T findOnly(Class<T> clazz) {
+        return (T) getOnlyElement(em().createQuery("FROM " + clazz.getSimpleName()).getResultList());
+    }
 
     @Before
     public void cleanDb() {
